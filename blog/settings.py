9aeb78a89e3f9280782f514d11d3e0a5693144d3
Adrian Borkowski <adbor88@gmail.com>
2017-04-22 16:52:41 +0200

    Added a Procfile.

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,17 @@ STATIC_URL = '/static/'
 
 GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = 'client_secret.json'
 GOOGLE_DRIVE_STORAGE_SERVICE_EMAIL = 'adbor88@gmail.com'
+
+
+# Heroku
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
