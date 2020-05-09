35f5d1c385c7115659b6e002261a07a861e4b032
adrianborkowski <adbor88@gmail.com>
2020-05-09 07:58:10 +0200

    update OpenLayer map

--- wall/coordinates.py
+++ wall/coordinates.py
@@ -16,15 +16,15 @@ def get_exif(fn):
 def add_coordinates():
     post = Post.objects.last()
     metadata = get_exif(post.image)
+    if 'GPSInfo' in metadata:
+        def dms2dd(i):
+            decs = float(i[0][0])
+            mins = float(i[1][0])/60
+            secs = float(i[2][0])/36000000
+            return decs + mins + secs
+        post.lat = dms2dd(metadata['GPSInfo'][2])
+        post.lon = dms2dd(metadata['GPSInfo'][4])
+        post.save()
 
-    def dms2dd(i):
-        decs = float(i[0][0])
-        mins = float(i[1][0])/60
-        secs = float(i[2][0])/36000000
-        return decs + mins + secs
-
-    post.lat = dms2dd(metadata['GPSInfo'][2])
-    post.lon = dms2dd(metadata['GPSInfo'][4])
-    post.save()
 
 
